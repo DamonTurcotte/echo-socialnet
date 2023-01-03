1c4dabb7a89416f96ada134ba9e32ce5d337f74b
altagrave <damon.j.turcotte@gmail.com>
2023-01-02 23:47:33 -0700

    add avatar, bio, uuid to user model

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
+import uuid
+
+
+class CustomUser(AbstractUser):
+    uuid = models.UUIDField(default=uuid.uuid4)
+    bio = models.TextField(max_length=150, null=True, blank=True)
+    avatar = models.ImageField(upload_to='static/users/', null=True, blank=True)
+
+    class Meta:
+        ordering = []
+
+    def __str__(self):
+        return self.username
