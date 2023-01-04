019604c40d2b28be4381b8923810695280b8c3eb
altagrave <damon.j.turcotte@gmail.com>
2023-01-03 23:57:02 -0700

    add absolute url, ordering | model name

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.CustomUser)
+admin.site.register(models.EchoUser)
--- users/models.py
+++ users/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.hashers import make_password
+from django.urls import reverse
 import uuid
 
 
-class CustomUser(AbstractUser):
+class EchoUser(AbstractUser):
     uuid = models.UUIDField(default=uuid.uuid4)
     bio = models.TextField(max_length=150, null=True, blank=True)
     avatar = models.ImageField(default='static/users/default.png', upload_to='static/users/', null=True, blank=True)
 
     class Meta:
-        ordering = []
+        ordering = ['username', 'uuid', 'bio', 'avatar']
 
     def __str__(self):
         return self.username
+
+    def get_absolute_url(self):
+        return reverse("users:profile", kwargs={"uuid": self.uuid})
+    
