0cce4256dd03fdc4abcf2e2c836241a31c865fe7
altagrave <damon.j.turcotte@gmail.com>
2023-01-03 23:51:15 -0700

    add profile view

--- users/views.py
+++ users/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login
-from .forms import CustomUserCreationForm
-from .models import CustomUser
+from .forms import EchoUserCreationForm
+from .models import EchoUser
 from django.contrib.auth.decorators import login_required
 
 def signup_view(request):
 
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST, request.FILES)
+        form = EchoUserCreationForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect(reverse('home'))
 
     else:
-        form = CustomUserCreationForm()
+        form = EchoUserCreationForm()
     
     context = {
         'title': 'Sign Up | Echo',
@@ -23,9 +23,17 @@ def signup_view(request):
         }
 
     return render(request, 'users/signup.html', context=context)
-        
 
 
+def profile_view(request, uuid):
+    data = EchoUser.objects.get(uuid=uuid)
+    context = {
+        'title': f'{data.username} | Echo',
+        'profile': {
+            'username': data.username,
+            'bio': data.bio,
+            'avatar': data.avatar
+        }
+    }
 
-
-
+    return render(request, 'users/profile.html', context=context)
