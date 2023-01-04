d2ba8f40ae5c99b8febe51be032ae5d657915ecc
altagrave <damon.j.turcotte@gmail.com>
2023-01-03 23:52:51 -0700

    add user context "uuid"

--- echo/views.py
+++ echo/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home | Echo Social'
+        'title': 'Home | Echo Social',
+        'uuid': request.user.uuid
     }
     return render(request, 'echo/index.html', context=context)
