fb94a614f7604d789b3f61453aa7d7a55242f1a3
altagrave <damon.j.turcotte@gmail.com>
2023-01-08 20:48:37 -0700

    Add posts model

--- posts/models.py
+++ posts/models.py
@@ -2,4 +2,9 @@ from django.db import models
 from users.models import EchoUser
 
 class Posts(models.Model):
-    post = models.TextField(max_length=140)
+    app_name = 'posts'
+    
+    echouser = models.ForeignKey('users.EchoUser', on_delete=models.CASCADE)
+    post = models.TextField(max_length=280)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
