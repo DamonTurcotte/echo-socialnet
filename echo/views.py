0e5a91fb3e2f16aed5f0eecd4100b10eff45143f
altagrave <damon.j.turcotte@gmail.com>
2023-01-08 20:50:34 -0700

    Modify index view context

--- echo/views.py
+++ echo/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
+    try:
+        uuid = request.user.uuid
+    except: uuid = None
+
     context = {
         'title': 'Home | Echo Social',
-        'uuid': request.user.uuid
+        'uuid': uuid
     }
     return render(request, 'echo/index.html', context=context)
