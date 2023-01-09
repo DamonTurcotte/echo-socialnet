a819a1cd21e4372152285d9e61164ff6e05f6ffa
altagrave <damon.j.turcotte@gmail.com>
2023-01-08 20:49:16 -0700

    Change upload path | add app name

--- users/models.py
+++ users/models.py
@@ -5,16 +5,15 @@ import uuid
 
 
 class EchoUser(AbstractUser):
+    app_name = 'users'
+
+    id = models.BigAutoField(primary_key=True)
     uuid = models.UUIDField(default=uuid.uuid4)
     bio = models.TextField(max_length=150, null=True, blank=True)
-    avatar = models.ImageField(default='static/users/default.png', upload_to='static/users/', null=True, blank=True)
+    avatar = models.ImageField(default='users/default.png', upload_to='users/', null=True, blank=True)
 
     class Meta:
         ordering = ['username', 'uuid', 'bio', 'avatar']
 
     def __str__(self):
         return self.username
-
-    def get_absolute_url(self):
-        return reverse("users:profile", kwargs={"uuid": self.uuid})
-    
