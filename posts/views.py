cb24784016ec4ba9fe2b68b0e96e514dc2958970
altagrave <damon.j.turcotte@gmail.com>
2023-01-13 23:25:47 -0700

    add create post view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .models import Post
 
-# Create your views here.
+
+def create_post(request):
+    if request.method == 'POST':
+        post = request.POST['post']
+        echouser = request.user
+        Post.objects.create(post=post, echouser=echouser)
+        
+        return redirect(reverse('home'))
+        
+
+    context = {
+        'title': 'New Post | Echo',
+    }
+    return render(request, 'posts/create_post.html', context=context)
