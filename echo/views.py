0a3c0ef1b867f3b6d1b3a452a03fb9b03e43e32b
altagrave <damon.j.turcotte@gmail.com>
2023-01-20 00:16:02 -0700

    Mv ajax view w/ various additions: Add  json response for user auth, profile tabs, post replies Change json response data object catergories

--- echo/views.py
+++ echo/views.py
@@ -2,12 +2,106 @@ from django.shortcuts import render
 from posts.models import Post
 from users.models import EchoUser
 from django.http import JsonResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
 def index(request):
-    posts = Post.objects.order_by('timestamp').reverse().all()
+    posts = Post.objects.order_by('timestamp').filter(reply_to=None).reverse().all()
+
+    post_list = list()
+    for post in posts:
+        post_view = dict()
+        post_view['post'] = post
+        if post.likes.filter(username=request.user).exists():
+            post_view['liked'] = ' active'
+        else:
+            post_view['liked'] = ''
+        post_list.append(post_view)
+
+    context = {
+        'title': 'Home | Echo Social',
+        'post_list': post_list
+    }
+    return render(request, 'echo/index.html', context=context)
+
+
+@ensure_csrf_cookie
+def ajax_response(request):
+    if request.method == 'GET':
+        print(request.GET)
+
+        if request.GET['action'] == 'auth':
+            if request.user.is_authenticated:
+                return JsonResponse({'status': 'user'})
+
+            else:
+                return JsonResponse({'status': 'guest'})
+
+        posts = Post.objects.all()
+        echouser = request.user
+        post_list = list()
+
+        # RETURN PROFILE TAB POSTS
+
+        if request.GET['action'] == 'get_profile_replies':
+            echouser = EchoUser.objects.get(username=request.GET['instance'])
+            posts = Post.objects.order_by('timestamp').reverse().filter(
+                echouser=echouser.id).all()
+            posts = posts.exclude(reply_to=None)
+
+        if request.GET['action'] == 'get_profile_posts':
+            echouser = EchoUser.objects.get(username=request.GET['instance'])
+            posts = Post.objects.order_by('timestamp').reverse().filter(
+                echouser=echouser.id).all()
+            posts = posts.filter(reply_to=None)
+
+        if request.GET['action'] == 'get_profile_likes':
+            echouser = EchoUser.objects.get(username=request.GET['instance'])
+            posts = Post.objects.order_by('timestamp').reverse().filter(
+                echouser=echouser.id).all()
+            posts = echouser.liked.order_by('timestamp').reverse().all()  # type:ignore
+
+        data = {
+            'post_list': post_list,
+            'status': 'posts_retrieved'
+        }
+
+        if request.GET['action'] == 'get_post_replies':
+            post_uuid = request.GET['instance']
+            posts = Post.objects.get(uuid=post_uuid)
+            posts = posts.replies.all()  # type:ignore
+            data['status'] = 'replies_retrieved'
+
+        for post in posts:
+            post_view = dict()
+            post_view['echouser'] = post.echouser.username
+            post_view['echouser_id'] = post.echouser.uuid
+            post_view['avatar'] = post.echouser.avatar.url
+            post_view['post_id'] = post.uuid
+            post_view['post'] = post.post
+            post_view['when_posted'] = post.when_posted()
+            post_view['num_likes'] = post.num_likes()
+            post_view['num_replies'] = post.num_replies()
+
+            try:
+                post_view['reply_to_id'] = post.reply_to.uuid#type:ignore
+                post_view['reply_to_name'] = post.reply_to.echouser.username#type:ignore
+            except:
+                post_view['reply_to_id'] = ''
+                post_view['reply_to_name'] = ''
+
+            if post.likes.filter(username=request.user).exists():
+                post_view['liked'] = ' active'
+
+            else:
+                post_view['liked'] = ''
+
+            post_list.append(post_view)
+
+        return JsonResponse(data)
+
     if request.method == 'POST':
-        post = Post.objects.get(uuid=request.POST['post'])
+        post = Post.objects.get(uuid=request.POST['instance'])
         uuid = post.uuid
 
         if request.POST['action'] == 'like':
@@ -28,18 +122,4 @@ def index(request):
                 }
                 return JsonResponse(data)
 
-    post_list = list()
-    for post in posts:
-        post_view = dict()
-        post_view['post'] = post
-        if post.likes.filter(username=request.user).exists():
-            post_view['liked'] = ' active'
-        else:
-            post_view['liked'] = ''
-        post_list.append(post_view)
-
-    context = {
-        'title': 'Home | Echo Social',
-        'post_list': post_list
-    }
-    return render(request, 'echo/index.html', context=context)
+    return render(request, '404.html')
