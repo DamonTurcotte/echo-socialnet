4d09411ee413e23bab9bc7bac5f709843d5b4a4e
altagrave <damon.j.turcotte@gmail.com>
2023-01-20 00:33:51 -0700

    Add email, date joined, last login to profile view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login
 from .forms import EchoUserCreationForm
 from .models import EchoUser
 from django.contrib.auth.decorators import login_required
@@ -24,16 +24,21 @@ def signup_view(request):
 
     return render(request, 'users/signup.html', context=context)
 
+
 @login_required
 def profile_view(request, uuid):
-    data = EchoUser.objects.get(uuid=uuid)
+    profile = EchoUser.objects.get(uuid=uuid)
+
     context = {
-        'title': f'{data.username} | Echo',
+        'title': f'{profile.username} | Echo',
         'profile': {
-            'username': data.username,
-            'bio': data.bio,
-            'avatar': data.avatar
-        }
+            'username': profile.username,
+            'bio': profile.bio,
+            'avatar': profile.avatar,
+            'email': profile.email,
+            'date_joined': profile.date_joined.date(),
+            'last_login': profile.since_last_login()
+        },
     }
 
     return render(request, 'users/profile.html', context=context)
