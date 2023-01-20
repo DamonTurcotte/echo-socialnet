ab61cd51ebf3fe7b6f86f0fa88fb1f0094ff9908
altagrave <damon.j.turcotte@gmail.com>
2023-01-19 23:23:38 -0700

    Add reply creation view

--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,40 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.http import JsonResponse
 from .models import Post
+
 
 
 def create_post(request):
     if request.method == 'POST':
         post = request.POST['post']
         echouser = request.user
-        Post.objects.create(post=post, echouser=echouser)
-        
-        return redirect(reverse('home'))
-        
-
+        try:
+            reply_to = request.POST['reply_to']
+            reply_to = Post.objects.get(uuid=reply_to)
+            Post.objects.create(post=post, echouser=echouser, reply_to=reply_to)
+            return redirect(request.POST['newpath'])
+        except:
+            Post.objects.create(post=post, echouser=echouser)
+            return redirect(reverse('home'))
+    
     context = {
         'title': 'New Post | Echo',
     }
     return render(request, 'posts/create_post.html', context=context)
+
+
+def create_reply(request):
+    if request.method == 'POST':
+        post = request.POST['post']
+        reply_to = request.POST['reply_to']
+        echouser = request.user
+        Post.objects.create(post=post, reply_to=reply_to, echouser=echouser)
+
+        data = {}
+
+        return JsonResponse(data)
+    
+    else: return render(request, '404.html')
+
+
