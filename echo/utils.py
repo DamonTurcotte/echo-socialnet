7c2b008158b8a3a160093678cf2c65398688a77b
altagrave <damon.j.turcotte@gmail.com>
2023-02-01 21:35:59 -0700

    Move post_list construction to utils

--- echo/utils.py
+++ echo/utils.py
@@ -56,3 +56,64 @@ def time_between(date):
             return '1 year ago'
         else:
             return f'{years} years ago'
+        
+
+def make_post_objects(posts_queryset, request_username):
+    post_list = list()
+
+    for post in posts_queryset:
+        post_view = dict()
+        post_view['echouser'] = post.echouser.username
+        post_view['echouser_id'] = post.echouser.uuid
+        post_view['avatar'] = post.echouser.avatar.url
+        post_view['post_id'] = post.uuid
+        post_view['post'] = post.post
+        post_view['when_posted'] = post.when_posted()
+        post_view['num_likes'] = post.num_likes()
+        post_view['num_replies'] = post.num_replies()
+        post_view['num_reposts'] = post.num_reposts()
+        post_view['object'] = ['post']
+
+        alt_post_fields = [
+             'reply_to_id',
+              'reply_to_name',
+              'repost_id',
+              'repost_avatar',
+              'repost_user',
+              'repost_time',
+              'repost_post'
+        ]
+
+        for field in alt_post_fields:
+             post_view[field] = ''
+
+        try:
+            post_view['reply_to_id'] = post.reply_to.uuid  # type:ignore
+            # type:ignore
+            post_view['reply_to_name'] = post.reply_to.echouser.username
+            post_view['object'] = 'reply'
+        except:
+            pass
+
+        try:
+            post_view['repost_id'] = post.repost_of.uuid  # type:ignore
+            # type:ignore
+            post_view['repost_avatar'] = post.repost_of.echouser.avatar.url
+            # type:ignore
+            post_view['repost_user'] = post.repost_of.echouser.username
+            # type:ignore
+            post_view['repost_time'] = post.repost_of.when_posted()
+            post_view['repost_post'] = post.repost_of.post  # type:ignore
+            post_view['object'] = 'repost'
+        except:
+            pass
+
+        if post.likes.filter(username=request_username).exists():
+            post_view['liked'] = ' active'
+
+        else:
+            post_view['liked'] = ''
+
+        post_list.append(post_view)
+
+    return post_list
