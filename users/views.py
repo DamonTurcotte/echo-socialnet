e0957b4d07ba469981d66b6f89870184c3545338
altagrave <damon.j.turcotte@gmail.com>
2023-02-01 22:10:51 -0700

    Mod signup and profile views: (below) Signup view: add forked path boolean for if avatar uploaded, pillow resize and crop avatar, alter user form save for custom webp avatar gen Profile view: add to context  self, non-self profile recognition, current follow status of non-self profile

--- users/views.py
+++ users/views.py
@@ -1,18 +1,56 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth import login
+from django.core.files.base import ContentFile
 from .forms import EchoUserCreationForm
-from .models import EchoUser
+from .models import EchoUser, Follow
 from django.contrib.auth.decorators import login_required
+from PIL import Image
+import math
+from io import BytesIO
 
 def signup_view(request):
 
     if request.method == 'POST':
         form = EchoUserCreationForm(request.POST, request.FILES)
+        
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect(reverse('home'))
+            try:
+                print(request.FILES['avatar'])
+                upload = True
+            except:
+                upload = False
+
+            if upload == True:
+                x = int(math.floor(float(request.POST['x'])))
+                y = int(math.floor(float(request.POST['y'])))
+                width = int(math.floor(float(request.POST['width'])))
+                height = int(math.floor(float(request.POST['height'])))
+                dimensions = int(math.floor(float(request.POST['dimensions'])))
+
+                image = Image.open(request.FILES['avatar'])
+                resized_image = image.resize((width, height), Image.ANTIALIAS)
+                new_image = resized_image.crop((x, y, dimensions+x, dimensions+y))
+
+                new_image_io = BytesIO()
+                new_image.save(new_image_io, format='webp')
+
+                user = form.save(commit=False)
+                user.avatar.delete(save=False)
+                temp_name = f'{user.uuid}.webp'
+                user.avatar.save(
+                    temp_name,
+                    content=ContentFile(new_image_io.getvalue()),
+                    save=False
+                )
+                user.save()
+                login(request, user)
+                return redirect(reverse('home'))
+            
+            else:
+                user = form.save()
+                login(request, user)
+                return redirect(reverse('home'))
 
     else:
         form = EchoUserCreationForm()
@@ -28,6 +66,16 @@ def signup_view(request):
 @login_required
 def profile_view(request, uuid):
     profile = EchoUser.objects.get(uuid=uuid)
+    follow_status = ''
+
+    if profile == request.user:
+        follow_status = 'self'
+
+    elif Follow.objects.filter(echouser=request.user, follow=profile).exists():
+        follow_status = 'unfollow'
+
+    else:
+        follow_status = 'follow'
 
     context = {
         'title': f'{profile.username} | Echo',
@@ -37,7 +85,10 @@ def profile_view(request, uuid):
             'avatar': profile.avatar,
             'email': profile.email,
             'date_joined': profile.date_joined.date(),
-            'last_login': profile.since_last_login()
+            'last_login': profile.since_last_login(),
+            'num_following': profile.num_following(),
+            'num_followers': profile.num_followers(),
+            'follow_status': follow_status
         },
     }
 
