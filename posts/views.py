fc7132dd8a7bfde8d69a567ff0d8f75415c59604
altagrave <damon.j.turcotte@gmail.com>
2023-02-01 21:44:44 -0700

    Add repost, reply to create post; temp post detail

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import JsonResponse
 from .models import Post
+from django.views.generic import DetailView
+from echo.utils import make_post_objects
 
 
 
@@ -9,11 +11,22 @@ def create_post(request):
     if request.method == 'POST':
         post = request.POST['post']
         echouser = request.user
+
         try:
-            reply_to = request.POST['reply_to']
-            reply_to = Post.objects.get(uuid=reply_to)
-            Post.objects.create(post=post, echouser=echouser, reply_to=reply_to)
-            return redirect(request.POST['newpath'])
+            try:
+                repost_of = request.POST['repost_of']
+                repost_of = Post.objects.get(uuid=repost_of)
+                Post.objects.create(post=post, echouser=echouser, repost_of=repost_of)
+
+                return redirect(request.POST['newpath'])
+
+            except:
+                reply_to = request.POST['reply_to']
+                reply_to = Post.objects.get(uuid=reply_to)
+                Post.objects.create(post=post, echouser=echouser, reply_to=reply_to)
+
+                return redirect(request.POST['newpath'])
+        
         except:
             Post.objects.create(post=post, echouser=echouser)
             return redirect(reverse('home'))
@@ -24,17 +37,24 @@ def create_post(request):
     return render(request, 'posts/create_post.html', context=context)
 
 
-def create_reply(request):
-    if request.method == 'POST':
-        post = request.POST['post']
-        reply_to = request.POST['reply_to']
-        echouser = request.user
-        Post.objects.create(post=post, reply_to=reply_to, echouser=echouser)
+def post_detail_view(request, uuid):
+    post_list = list()
+    main_post = Post.objects.get(uuid=uuid)
+    post_list.append(main_post)
 
-        data = {}
+    if main_post.reply_to != None:
+        reply_to = main_post.reply_to
+        post_list.append(reply_to)
 
-        return JsonResponse(data)
+    if main_post.replies.exists():#type:ignore
+        for reply in main_post.replies.all():#type:ignore
+            post_list.append(reply)
+
+    post_list = make_post_objects(post_list, request.user)
     
-    else: return render(request, '404.html')
-
-
+    context = {
+        'title': 'Post | Echo',
+        'post_list': post_list
+        }
+    
+    return render(request, 'posts/post_detail.html', context=context)
