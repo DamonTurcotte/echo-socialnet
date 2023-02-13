bc71234752e215438f08fb5059064624a40a6c9b
altagrave <damon.j.turcotte@gmail.com>
2023-02-12 21:24:52 -0700

    Add fields to Alerts model

--- notifications/models.py
+++ notifications/models.py
@@ -1,6 +1,20 @@
 from django.db import models
-from users.models import EchoUser
 
 
 class Alerts(models.Model):
-    pass
+    recipient = models.ForeignKey('users.EchoUser', on_delete=models.CASCADE, related_name='notifs')
+    by_user = models.ForeignKey('users.EchoUser', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    follow = models.ForeignKey('users.Follow', on_delete=models.CASCADE, blank=True, null=True)
+    post = models.ForeignKey('posts.Post', on_delete=models.CASCADE, blank=True, null=True)
+
+    ALERT_TYPE = (
+        ('follow', 'follow'),
+        ('reply', 'reply'),
+        ('repost', 'repost'),
+        ('like', 'like')
+    )
+
+    type = models.CharField(max_length=7, choices=ALERT_TYPE)
