5e70a446a0cc973b7ec62bbbfb5b663daed6b2d8
altagrave <damon.j.turcotte@gmail.com>
2023-03-03 21:55:54 -0700

    Mod article __str__

--- news/models.py
+++ news/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+import uuid
 
 
 class Articles(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4)
     source = models.CharField(max_length=100)
     title = models.CharField(max_length=300)
     description = models.CharField(max_length=300, blank=True, null=True)
+    content = models.CharField(max_length=300, blank=True, null=True)
     url = models.URLField()
     image = models.URLField()
     date = models.DateField()
@@ -12,7 +15,7 @@ class Articles(models.Model):
     when_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.title[:30]}... | {self.date}'
+        return f'{self.title[:40]}... | {self.date}'
     
 class LastNewsUpdate(models.Model):
     time = models.DateTimeField()
