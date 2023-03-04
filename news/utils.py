5039ceb67d5e045eb58604087cadf115aa00435d
altagrave <damon.j.turcotte@gmail.com>
2023-03-03 21:57:29 -0700

    Add random page return, regex content editor

--- news/utils.py
+++ news/utils.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 import random
 import urllib.request
 import json
+import re
 
 
 def retrieve_articles():
@@ -29,11 +30,19 @@ def retrieve_articles():
                 url = article['url']
                 image = article['image']
 
+                # if content is truncated, remove trunc. tag from content string
+                content_str = str(article['content'])
+                if re.search('\\[[0-9]*\\s*chars\\]', content_str):
+                    regex = re.search('\\[[0-9]*\\s*chars*\\]', content_str)
+                    pos = regex.start()#type:ignore
+                    content_str = content_str[:pos]
+                content = content_str
+
                 pubdate = article['publishedAt'].split('T')[0].split('-')
                 [year, month, day] = [int(pubdate[0]), int(pubdate[1]), int(pubdate[2])]
                 date = datetime(year, month, day)
 
-                if Articles.objects.filter(url=url).exists():
+                if Articles.objects.filter(title=title).exists() or Articles.objects.filter(url=url).exists():
                     continue
                 else:
                     try:
@@ -41,6 +50,7 @@ def retrieve_articles():
                             source=source,
                             title=title,
                             description=description,
+                            content=content,
                             url=url,
                             image=image,
                             date=date,
@@ -61,6 +71,14 @@ def retrieve_articles():
     for obj in current_articles:
         article_list.append(obj)
 
-    articles_to_render = random.choices(article_list, k=3)
+    articles_to_render = []
+    i = 0
+    while i < 3:
+        try:
+            rand_article = random.choice(article_list)
+            articles_to_render.append(rand_article)
+            article_list.remove(rand_article)
+            i += 1
+        except: break
 
     return articles_to_render
