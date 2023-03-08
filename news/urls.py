581a3b45f7d8217a99832de42a7add5829c0fc75
altagrave <damon.j.turcotte@gmail.com>
2023-03-08 00:35:45 -0700

    Add browse search url

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from news import views
 
+app_name = 'browse'
+
 urlpatterns = [
+    path('search/', views.browse_search, name='search'),
     path('<category>/<uuid>/', views.article_detail, name='article'),
-    path('<category>/', views.browse_view, name='browse'),
+    path('<category>/', views.browse_view, name='category'),
     path('', views.browse_default)
 ]
