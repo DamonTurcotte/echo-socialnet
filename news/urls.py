37b4386d4b153c53c83661e7560a78d65e59d70b
altagrave <damon.j.turcotte@gmail.com>
2023-03-11 17:40:22 -0700

    add search results urls

--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,8 @@ from news import views
 app_name = 'browse'
 
 urlpatterns = [
-    path('search/', views.browse_search, name='search'),
+    path('search/<category>/', views.browse_search_results, name='search'),
+    path('search/', views.browse_search),
     path('<category>/<uuid>/', views.article_detail, name='article'),
     path('<category>/', views.browse_view, name='category'),
     path('', views.browse_default)
