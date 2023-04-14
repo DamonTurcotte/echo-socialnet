c3ae68a066a57f84cc22e3663a5803eb2215def8
altagrave <damon.j.turcotte@gmail.com>
2023-04-14 00:44:49 -0600

    Change media root for nginx

--- echo/settings.py
+++ echo/settings.py
@@ -114,7 +114,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+if DEBUG:
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+else:
+    MEDIA_ROOT = '/media/'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
