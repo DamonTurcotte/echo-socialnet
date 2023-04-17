b1e02d3ea5ef0a41d089de313ce594cf54a14e7b
altagrave <damon.j.turcotte@gmail.com>
2023-04-16 22:50:22 -0600

    Add custom validator for avatar return

---
+++ echo/oauth2_validators.py
@@ -0,0 +1,12 @@
+from oauth2_provider.oauth2_validators import OAuth2Validator
+
+class CustomOAuth2Validator(OAuth2Validator):
+    # "None" ignores scopes that limit claims to return, otherwise standard scopes are used.
+    oidc_claim_scope = None
+    def get_additional_claims(self, request):
+        claims = {
+            "username": request.user.username,
+            "avatar": request.user.avatar.url
+        }
+
+        return claims
