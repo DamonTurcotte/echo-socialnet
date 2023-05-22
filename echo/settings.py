edd659c58256b794077f2ea282c55d1f2a2beeed
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-22 01:02:15 -0600

    Mod media root

--- echo/settings.py
+++ echo/settings.py
@@ -145,10 +145,7 @@ else:
 
 MEDIA_URL = '/media/'
 
-if DEBUG:
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-else:
-    MEDIA_ROOT = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
