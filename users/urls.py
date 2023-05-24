8eea3d8b632b7519646482ea95e6fb27bbb4fa88
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-23 21:42:11 -0600

    Add remote user request endpoint

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('profile/<uuid>/', views.profile_view, name='profile'),
     path('user_data/', views.EchoUserResourceView.as_view(), name='user_data'),
+    path('get_user_data/', views.SpecifyEchoUserResourceView.as_view(), name='get_user_data'),
 ]
