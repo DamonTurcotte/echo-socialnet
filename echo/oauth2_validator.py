f017ce433f4512c9001079cbb6395e2badbcddbe
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-25 23:49:49 -0600

    Add custom user model fields to oidc claims

---
+++ echo/oauth2_validator.py
@@ -0,0 +1,17 @@
+from oauth2_provider.oauth2_validators import OAuth2Validator
+
+class CustomValidator(OAuth2Validator):
+    oidc_claim_scope = None
+
+    def get_additional_claims(self, request):
+        return {
+            "given_name": request.user.first_name,
+            "family_name": request.user.last_name,
+            "email": request.user.email,
+            "preferred_username": request.user.username,
+            "picture": request.user.avatar,
+        }
+    
+    def get_userinfo_claims(self, request):
+        claims = super().get_userinfo_claims(request)
+        return claims
