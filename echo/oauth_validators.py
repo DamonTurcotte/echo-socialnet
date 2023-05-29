2da7c1bd129553a2913c6147ab82c0b3b8cc83b1
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-28 20:41:48 -0600

    Add clear expired tokens

--- echo/oauth_validators.py
+++ echo/oauth_validators.py
@@ -1,10 +1,12 @@
 from oauth2_provider.oauth2_validators import OAuth2Validator
 from .settings import BASE_URL
+from oauth2_provider.models import clear_expired
 
 class CustomOAuth2Validator(OAuth2Validator):
     oidc_claim_scope = None
 
     def get_additional_claims(self, request):
+        clear_expired()
         return {
             "given_name": request.user.first_name,
             "family_name": request.user.last_name,
