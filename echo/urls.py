62aa13e9ffb466cffce602e33efc7cfa770baf59
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-07-01 03:30:13 -0600

    Remove unused import

--- echo/urls.py
+++ echo/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -37,8 +37,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# urlpatterns += [
-#     re_path(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-#     re_path(r'^profile/$', views.profile),
-# ]
