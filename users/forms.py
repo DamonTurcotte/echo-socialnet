5fc1ee197087c97d746ba1f6c587792dee2fac48
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-09-08 09:00:19 -0600

    Add recaptcha widgets to user forms

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 from .models import EchoUser
+from captcha.fields import ReCaptchaField
+from echo.settings import RECAPTCHA_PRIVATE_KEY, RECAPTCHA_PUBLIC_KEY
+from captcha.widgets import ReCaptchaV3
+from django.contrib.auth.forms import AuthenticationForm
 
 class EchoUserCreationForm(forms.ModelForm):
+    captcha = ReCaptchaField(widget=ReCaptchaV3)
+
     class Meta:
         model = EchoUser
         fields = ['username', 'password', 'bio', 'avatar']
         widgets = { 'password': forms.PasswordInput() }
+
+class CustomAuthenticationForm(AuthenticationForm):
+    captcha = ReCaptchaField(widget=ReCaptchaV3)
