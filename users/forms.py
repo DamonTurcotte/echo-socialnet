5bcfad5fd71ab57295a5acf8c2486aa20659b7ca
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-09-09 00:15:16 -0600

    Add custom auth form for recaptcha

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from .models import EchoUser
+from django.contrib.auth.forms import AuthenticationForm
+from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV2Checkbox
 
 class EchoUserCreationForm(forms.ModelForm):
     class Meta:
         model = EchoUser
         fields = ['username', 'password', 'bio', 'avatar']
         widgets = { 'password': forms.PasswordInput() }
+
+class EchoUserLoginForm(AuthenticationForm):
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
